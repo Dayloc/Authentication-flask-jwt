edef1c532bdf88a05fcb34b2b178a60e44446ca8
Dayloc <158208881+Dayloc@users.noreply.github.com>
2025-03-06 18:47:32 +0000

    Se ha configurado la libreria para el token y ya se puede ver el token

--- src/api/routes.py
+++ src/api/routes.py
@@ -5,6 +5,7 @@ from flask import Flask, request, jsonify, url_for, Blueprint
 from api.models import db, User
 from api.utils import generate_sitemap, APIException
 from flask_cors import CORS
+from flask_jwt_extended import create_access_token, get_jwt_identity
 
 api = Blueprint('api', __name__)
 
@@ -20,3 +21,22 @@ def handle_hello():
     }
 
     return jsonify(response_body), 200
+
+
+@api.route('/token', methods=['POST'])
+def handle_token():
+    email = request.json.get('email')
+    password = request.json.get('password')
+
+    if email is None:
+        return jsonify({"msg": "Email is required"}), 400
+    if password is None:
+        return jsonify({"msg": "Password is required"}), 400
+
+    # Asegúrate de que `password` sea una cadena de texto
+    password = str(password)
+
+    user = User.query.filter_by(email=email, password=password).first()
+    user = user.serialize()
+    token = create_access_token(identity=user)
+    return jsonify({'token': token, "user": user}), 200
