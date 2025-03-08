613665dc60d150f9bfe641f94d014384032345db
Dayloc <158208881+Dayloc@users.noreply.github.com>
2025-03-08 10:05:38 +0000

    Agregado en el Layout Rergister

--- src/front/pages/Layout.jsx
+++ src/front/pages/Layout.jsx
@@ -2,13 +2,15 @@ import { Outlet } from "react-router-dom/dist"
 import ScrollToTop from "../components/ScrollToTop"
 import { Navbar } from "../components/Navbar"
 import { Footer } from "../components/Footer"
+import Register from "../components/Register"
 
 // Base component that maintains the navbar and footer throughout the page and the scroll to top functionality.
 export const Layout = () => {
     return (
         <ScrollToTop>
             <Navbar />
-                <Outlet />
+            <Outlet />
+            <Register />
             <Footer />
         </ScrollToTop>
     )
