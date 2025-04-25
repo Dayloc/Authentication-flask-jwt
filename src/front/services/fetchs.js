e40c84fe11d586df6158f610d8aae6b2e9ef7f65
Dayloc <daylocyumar@gmail.com>
2025-04-25 17:34:04 +0000

    Configuración corregida

---
+++ src/front/services/fetchs.js
@@ -0,0 +1,64 @@
+export const login = async (dispatch, email, password) => {
+  try {
+    const response = await fetch(
+      "https://cautious-space-memory-4jj4xwj6pqvq26w6-3001.app.github.dev/api/token",
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ email, password }),
+      }
+    );
+
+    // Verificar si la respuesta es exitosa
+    if (!response.ok) {
+      const errorData = await response.json();
+      console.error("Error en el login:", errorData);
+      return { error: errorData.message || "Usuario no registrado, por favor registrese." };
+    }
+
+    const data = await response.json();
+    console.log("Respuesta del servidor:", data);
+
+    if (data.token) {
+      // Guardar el token en el store
+      dispatch({ type: "set_token", payload: data.token });
+      localStorage.setItem("token", data.token);
+      return data;
+      
+    } else {
+      return { error: "No se recibió un token válido." };
+    }
+  } catch (error) {
+    console.error("Error en la solicitud de login:", error);
+    return { error: "No se pudo conectar con el servidor." };
+  }
+};
+export const register = async (dispatch, email, password, isActive) => {
+  try {
+    const response = await fetch(
+      "https://cautious-space-memory-4jj4xwj6pqvq26w6-3001.app.github.dev/api/register",
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ email, password, isActive }),
+      }
+    );
+
+    // Verificar si la respuesta es exitosa
+    if (!response.ok) {
+      const errorData = await response.json();
+      console.error("Error en el login:", errorData);
+      return { error: errorData.message || "Error desconocido en login" };
+    }
+
+    const data = await response.json();
+    console.log("Respuesta del servidor:", data);
+  } catch (error) {
+    console.error("Error en la solicitud de login:", error);
+    return { error: "No se pudo conectar con el servidor." };
+  }
+};
